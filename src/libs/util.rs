1d66d57f2ae06bfd579cc2403f3d4aa536647ad2
uptu <bh1000@live.com>
2023-02-03 13:47:00 -0700

    Day 2: continue util ports and attempt to fix main.rs

--- src/libs/util.rs
+++ src/libs/util.rs
@@ -1,6 +1,10 @@
 extern crate nalgebra_glm as glm;
+
 use glm::{DVec3, TVec};
 
+/// **normalize**
+/// 
+/// See xq_norm()
 fn normalize(x: &DVec3) -> DVec3 {
     let nrm: DVec3 = norm(x);
     let cln:DVec3 = DVec3::new(x[0], x[1], x[2]);
@@ -8,14 +12,37 @@ fn normalize(x: &DVec3) -> DVec3 {
     return DVec3::new(cln[0]/nrm[0], cln[1]/nrm[1], cln[2]/nrm[2]);
 }
 
+/// **xq_norm**
+/// 
+/// Runs a vector for `x / norm(x)`, which returns `x` multiplied by the inverse of normalized `x`.
+fn xq_norm(x: &DVec3) -> DVec3 {
+    normalize(x)
+}
+
+/// **norm_sq**
+/// 
+/// See dot_product()
 fn norm_sq(x: &DVec3) -> f64 {
     x.dot(x)
 }
 
+/// **dot_product**
+/// 
+/// Gets the dot product for a given vector.
+fn dot_product(x: &DVec3) -> f64 {
+    norm_sq(x)
+}
+
+/// **norm**
+/// 
+/// Gets a normalized vector.
 fn norm(x: &DVec3) -> DVec3 {
     return glm::normalize(x);
 }
 
+/// **get_sub_keys**
+/// 
+/// Gets sub keys pushed from GLSL.
 fn get_sub_keys<T, const R: usize>(v: &TVec<T, R>) -> Vec<String>
 where
     T: std::fmt::Display,
@@ -29,10 +56,16 @@ where
     ret
 }
 
-fn to_vec3_from_float(x: f64) -> DVec3 {
+/// **to_vec3_from_f64**
+/// 
+/// Returns a given 3D vector populated with a given `f64` in all 3 axes.
+fn to_vec3_from_f64(x: f64) -> DVec3 {
     glm::vec3(x, x, x)
 }
 
+/// **to_str**
+/// 
+/// Returns a boolean represented as a String.
 fn to_str(x: bool) -> String {
     if x {
         return String::from("1");
@@ -41,10 +74,16 @@ fn to_str(x: bool) -> String {
     }
 }
 
+/// **slice_to_string**
+/// 
+/// Returns a String populated by a `_` literal followed by the given string slice.
 fn slice_to_str(x: &str) -> String {
     return String::from("_") + x;
 }
 
+/// **vec3_str**
+/// 
+/// Returns a string representation of a 3D vector.
 fn vec3_str(x: &DVec3) -> String {
     String::from("vec3(")
         + &x[0].to_string()[..]
@@ -53,4 +92,32 @@ fn vec3_str(x: &DVec3) -> String {
         + ","
         + &x[2].to_string()[..]
         + ")"
+}
+/// **q_half**
+/// 
+/// Returns 1/2 of a given float in a much faster way than doing `x/2`.
+fn q_half(x:f64) -> f64{
+    let i:u64 = x.to_bits(); // Deconstruct to bits
+    f64::from_bits(i << 1)
+}
+
+/// **vec3_eq**
+/// 
+/// Returns true if vector `v` is populated with the same values as vector `w`.
+fn vec3_eq(v:DVec3, w:DVec3) -> bool{
+    let mut i = 0;
+    while i < 3 {
+        if v[i] != w[i] {
+            return false;
+        }
+        i += 1;
+    }
+    return true;
+}
+
+/// **smin**
+/// 
+/// Does some math; will update when I find context other than identical shader fn
+fn smin(a:f64, b:f64, k:f64) -> f64{
+    f64::min(f64::max(0.5 + q_half(b-a)/k, 0.0), 1.0)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use beryllium::{
     init::InitFlags,
     video::{CreateWinArgs, GlProfile},
-    *,
+    *, events::Event,
 };
 
 mod libs;
@@ -10,9 +10,12 @@ fn main() {
     /* Start SDL and run setup */
     let sdl = Sdl::init(InitFlags::EVERYTHING);
     /* Set context attributes */
-    sdl.set_gl_context_major_version(3);
-    sdl.set_gl_context_minor_version(3);
-    sdl.set_gl_profile(GlProfile::Core);
+    sdl.set_gl_context_major_version(3)
+        .expect("could not set OpenGl major version");
+    sdl.set_gl_context_minor_version(3)
+        .expect("could not set OpenGl minor version");
+    sdl.set_gl_profile(GlProfile::Core)
+        .expect("could not set OpenGl profile");
     #[cfg(target_os = "macos")]
     {
         sdl.set_gl_context_flags(ContextFlags::ForwardCompatibility);
@@ -31,4 +34,17 @@ fn main() {
     let _win = sdl
         .create_gl_window(_win_args)
         .expect("couldn't make a window and context");
+
+    'main_loop: loop {
+        // handle events this frame
+        while let Some(event) = sdl.poll_events().and_then(Result::ok) { // Doesn't work like tutorial???
+            match event {
+                Event::Quit => break 'main_loop,
+                _ => (),
+            }
+        }
+    // now the events are clear
+    
+    // here's where we could change the world state and draw.
+    }
 }
